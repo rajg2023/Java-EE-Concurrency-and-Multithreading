1d3d33fdf191ebf4dfe4891a445d26bb86497bdf
Rajiv Giri <rajivgiri513@gmail.com>
2022-01-30 04:03:12 -0500

    [Added Apache Tomcat V10 Server to the application]

--- javaeeconcurrency/src/main/java/com/rajivgiri/rest/GreetResources.java
+++ javaeeconcurrency/src/main/java/com/rajivgiri/rest/GreetResources.java
@@ -1,8 +1,13 @@
 package com.rajivgiri.rest;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("greetUser")
 public class GreetResources {
 
+	@GET
+	public String greetUser() {
+		return "Java EE concurrency starts!";
+	}
 }
